25568c4b49bca7dec6ca949334bbdc34784378c6
Veenaregoti <113594322+Veenaragoti@users.noreply.github.com>
2022-09-23 21:51:22 +0530

    Update code.java

--- code.java
+++ code.java
@@ -7,14 +7,12 @@ public class SwapNumbers {
         System.out.println("--Before swap--");
         System.out.println("First number = " + first);
         System.out.println("Second number = " + second);
-
-        // Value of first is assigned to temporary
         int temporary = first;
 
-        // Value of second is assigned to first
+        
         first = second;
 
-        // Value of temporary (which contains the initial value of first) is assigned to second
+        
         second = temporary;
 
         System.out.println("--After swap--");
